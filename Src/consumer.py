db1cdb5b2c4d4150ab1935eddcc382a6c6eedcc0
kipfuho <ctcakip@gmail.com>
2024-10-21 21:57:45 +0700

    feat: add data schema

--- Src/consumer.py
+++ Src/consumer.py
@@ -1,4 +1,19 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, DateType
+
+# Define the schema for your JSON data
+json_schema = StructType([
+    StructField("Ticker", StringType(), True),
+    StructField("Date", DateType(), True),
+    StructField("Open", DoubleType(), True),
+    StructField("High", DoubleType(), True),
+    StructField("Low", DoubleType(), True),
+    StructField("Close", DoubleType(), True),
+    StructField("Volume", DoubleType(), True),
+    StructField("Dividends", DoubleType(), True),
+    StructField("Stock Splits", DoubleType(), True),
+])
 
 # Create a Spark session
 spark = SparkSession.builder \
@@ -13,13 +28,15 @@ kafka_df = spark.readStream \
     .option("subscribe", "test-topic") \
     .load()
 
-# Convert the Kafka message value from binary to string
-messages_df = kafka_df.selectExpr("CAST(value AS STRING) as message")
+# Convert the Kafka message value from binary to string and parse JSON
+messages_df = kafka_df.selectExpr("CAST(value AS STRING) as json") \
+    .select(from_json(col("json"), json_schema).alias("data")) \
+    .select("data.*")  # Select fields from the parsed JSON
 
 # Write the messages to HDFS in text format
 query = messages_df.writeStream \
     .outputMode("append") \
-    .format("text") \
+    .format("parquet") \
     .option("path", "hdfs://namenode:9000/kafka_output") \
     .option("checkpointLocation", "/tmp/spark_checkpoint") \
     .start()
