54c22a691ca6a1d29d6795591c6fc35d88d0eb5e
kipfuho <ctcakip@gmail.com>
2024-10-21 19:21:34 +0700

    feat: docker file and worker example

---
+++ Src/consumer.py
@@ -0,0 +1,28 @@
+from pyspark.sql import SparkSession
+
+# Create a Spark session
+spark = SparkSession.builder \
+    .appName("KafkaSparkHDFSExample") \
+    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2") \
+    .getOrCreate()
+
+# Read from Kafka
+kafka_df = spark.readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("subscribe", "test-topic") \
+    .load()
+
+# Convert the Kafka message value from binary to string
+messages_df = kafka_df.selectExpr("CAST(value AS STRING) as message")
+
+# Write the messages to HDFS in text format
+query = messages_df.writeStream \
+    .outputMode("append") \
+    .format("text") \
+    .option("path", "hdfs://localhost:9000/kafka_output") \
+    .option("checkpointLocation", "/tmp/spark_checkpoint") \
+    .start()
+
+# Await termination of the stream
+query.awaitTermination()
---
+++ Src/producer.py
@@ -0,0 +1,26 @@
+from confluent_kafka import Producer
+import socket
+
+# Define Kafka producer configurations
+conf = {
+    'bootstrap.servers': "localhost:9092",  # Adjust this to your Kafka broker
+    'client.id': socket.gethostname()
+}
+
+# Create the Kafka Producer
+producer = Producer(conf)
+
+# Define a callback for delivery reports
+def delivery_report(err, msg):
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
+    else:
+        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+# Produce message
+for i in range(10):
+    message = f"Message {i} from Spark"
+    producer.produce('test-topic', message.encode('utf-8'), callback=delivery_report)
+
+# Wait for all messages to be delivered
+producer.flush()
