42674bd5cefe759ee2a91e9e07149f14ee0bd490
kipfuho <ctcakip@gmail.com>
2024-10-23 08:20:28 +0700

    feat: producer json

--- Src/producer.py
+++ Src/producer.py
@@ -1,3 +1,4 @@
+import json
 from confluent_kafka import Producer
 import socket
 
@@ -17,9 +18,36 @@ def delivery_report(err, msg):
     else:
         print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
 
-# Produce message
-for i in range(10):
-    message = f"Message {i} from Spark"
+# Sample data that matches the json_schema structure
+data = [
+    {
+        "Ticker": "AAPL",
+        "Date": "2024-10-22",
+        "Open": 174.55,
+        "High": 176.2,
+        "Low": 173.89,
+        "Close": 175.98,
+        "Volume": 68500000,
+        "Dividends": 0.0,
+        "Stock Splits": 0.0
+    },
+    {
+        "Ticker": "MSFT",
+        "Date": "2024-10-22",
+        "Open": 320.14,
+        "High": 322.45,
+        "Low": 319.12,
+        "Close": 321.78,
+        "Volume": 34200000,
+        "Dividends": 0.0,
+        "Stock Splits": 0.0
+    }
+]
+
+# Produce JSON message
+for record in data:
+    # Convert each record to a JSON string
+    message = json.dumps(record)
     producer.produce('test-topic', message.encode('utf-8'), callback=delivery_report)
 
 # Wait for all messages to be delivered
