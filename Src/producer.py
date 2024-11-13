e6a0bcf98a98431d7c11720b2913a1467a2df647
kipfuho <ctcakip@gmail.com>
2024-11-13 19:50:13 +0700

    feat: add merge file job

--- Src/producer.py
+++ Src/producer.py
@@ -1,4 +1,5 @@
 import json
+import os
 from confluent_kafka import Producer
 import socket
 
@@ -18,86 +19,8 @@ def delivery_report(err, msg):
     else:
         print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
 
-# Sample data that matches the json_schema structure
-data = [
-    {
-        "Ticker": "AAPL",
-        "Date": "2024-10-22",
-        "Open": 174.55,
-        "High": 176.2,
-        "Low": 173.89,
-        "Close": 175.98,
-        "Volume": 68500000,
-        "Dividends": 0.0,
-        "Stock Splits": 0.0
-    },
-    {
-        "Ticker": "MSFT",
-        "Date": "2024-10-22",
-        "Open": 320.14,
-        "High": 322.45,
-        "Low": 319.12,
-        "Close": 321.78,
-        "Volume": 34200000,
-        "Dividends": 0.0,
-        "Stock Splits": 0.0
-    },
-    {
-        "Ticker": "MSFT",
-        "Date": "2024-10-22",
-        "Open": 320.14,
-        "High": 322.45,
-        "Low": 319.12,
-        "Close": 321.78,
-        "Volume": 34200000,
-        "Dividends": 0.0,
-        "Stock Splits": 0.0
-    },
-    {
-        "Ticker": "MSFT",
-        "Date": "2024-10-22",
-        "Open": 320.14,
-        "High": 322.45,
-        "Low": 319.12,
-        "Close": 321.78,
-        "Volume": 34200000,
-        "Dividends": 0.0,
-        "Stock Splits": 0.0
-    },
-    {
-        "Ticker": "MSFT",
-        "Date": "2024-10-22",
-        "Open": 320.14,
-        "High": 322.45,
-        "Low": 319.12,
-        "Close": 321.78,
-        "Volume": 34200000,
-        "Dividends": 0.0,
-        "Stock Splits": 0.0
-    },
-    {
-        "Ticker": "MSFT",
-        "Date": "2024-10-22",
-        "Open": 320.14,
-        "High": 322.45,
-        "Low": 319.12,
-        "Close": 321.78,
-        "Volume": 34200000,
-        "Dividends": 0.0,
-        "Stock Splits": 0.0
-    },
-    {
-        "Ticker": "MSFT",
-        "Date": "2024-10-22",
-        "Open": 320.14,
-        "High": 322.45,
-        "Low": 319.12,
-        "Close": 321.78,
-        "Volume": 34200000,
-        "Dividends": 0.0,
-        "Stock Splits": 0.0
-    }
-]
+with open(os.path.join(os.path.dirname(__file__), "AAPL_stock_data.json"), "r") as json_file:
+    data = json.load(json_file)
 
 # Produce JSON message
 for record in data:
